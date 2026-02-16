7186dc3723022fda1edebbd08d2186b2094cc521
artemerofeev <ErofeevAN@bcs.ru>
2026-02-16 20:00:13 +0300

    koltushi functional

--- src/main/java/org/monkey_business/utility_supervisor/task/ScheduledTasks.java
+++ src/main/java/org/monkey_business/utility_supervisor/task/ScheduledTasks.java
@@ -1,16 +1,9 @@
 package org.monkey_business.utility_supervisor.task;
 
-import org.apache.http.HttpStatus;
 import org.monkey_business.utility_supervisor.dto.KoltushiOutageResponseDto;
-import org.monkey_business.utility_supervisor.dto.RossetiResultOutageDto;
-import org.monkey_business.utility_supervisor.dto.RossetiOutageResponseDto;
-import org.monkey_business.utility_supervisor.config.RossetiConfig;
 import org.monkey_business.utility_supervisor.config.TelegramBotConfig;
-import org.monkey_business.utility_supervisor.request.RossetiRequest;
 import org.monkey_business.utility_supervisor.service.KoltushiService;
-import org.monkey_business.utility_supervisor.service.RossetiService;
 import org.monkey_business.utility_supervisor.storage.KoltushiStorage;
-import org.monkey_business.utility_supervisor.storage.RossetiStorage;
 import org.monkey_business.utility_supervisor.telegram.BotMessageProcessor;
 import org.monkey_business.utility_supervisor.telegram.PowerOutageBot;
 import org.slf4j.Logger;
@@ -19,10 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -32,77 +25,26 @@ import java.util.stream.Collectors;
 public class ScheduledTasks {
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
-    private final RossetiService rossetiService;
     private final KoltushiService koltushiService;
     private final KoltushiStorage koltushiStorage;
     private final BotMessageProcessor processor;
     private final PowerOutageBot bot;
-    private final RossetiConfig rossetiConfig;
-    private final TelegramBotConfig telegramBotConfig;
-    private final String district;
-    private final String settlement;
     private final List<String> chatIds;
 
     @Autowired
-    public ScheduledTasks(RossetiService rossetiService, BotMessageProcessor processor, PowerOutageBot bot,
-                          RossetiConfig rossetiConfig, TelegramBotConfig telegramBotConfig,
-                          KoltushiService koltushiService, KoltushiStorage koltushiStorage) {
-        this.rossetiService = rossetiService;
-        this.processor = processor;
-        this.bot = bot;
-        this.rossetiConfig = rossetiConfig;
-        this.telegramBotConfig = telegramBotConfig;
+    public ScheduledTasks(KoltushiService koltushiService, KoltushiStorage koltushiStorage,
+                          BotMessageProcessor processor, PowerOutageBot bot,
+                          TelegramBotConfig telegramBotConfig) {
         this.koltushiService = koltushiService;
         this.koltushiStorage = koltushiStorage;
-        this.district = rossetiConfig.getDistrict();
-        this.settlement = rossetiConfig.getStreet();
+        this.processor = processor;
+        this.bot = bot;
         this.chatIds = telegramBotConfig.getChatIds();
     }
 
-    @Scheduled(cron = "${scheduling.cron.evening}", zone = "Europe/Moscow")
-    public void callEveningRosseti() {
-        log.info("Evening reminder schedule started");
-        LocalDate starDate = LocalDate.now().plusDays(1);
-        LocalDate endDate = starDate;
-        RossetiRequest request = new RossetiRequest(district, starDate, endDate, settlement);
-        RossetiResultOutageDto<RossetiOutageResponseDto> rossetiResultOutageDto = rossetiService.find(request, 1);
-        if (rossetiResultOutageDto.getData().size() > 0 && rossetiResultOutageDto.getStatusCode() == HttpStatus.SC_OK) {
-            RossetiStorage.put(rossetiResultOutageDto.getData().get(0).getStartDate(), rossetiResultOutageDto);
-            SendMessage message = processor.makeMessageForTomorrow(rossetiResultOutageDto);
-            for (String chatId : chatIds) {
-                log.info("callEvening chat id: " + chatId);
-                message.setChatId(chatId);
-                bot.sendMessage(message);
-            }
-        }
-        log.info("Evening reminder schedule finished");
-    }
-
-    @Scheduled(cron = "${scheduling.cron.morning}", zone = "Europe/Moscow")
-    public void callMorningRosseti() {
-        log.info("Morning reminder schedule started");
-        LocalDate date = LocalDate.now();
-        String formattedDate = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-        log.info("Morning reminder - get result outage dto");
-        RossetiResultOutageDto<RossetiOutageResponseDto> rossetiResultOutageDto = RossetiStorage.get(formattedDate);
-        if (rossetiResultOutageDto != null) {
-            SendMessage message = processor.makeMessageForToday(rossetiResultOutageDto);
-            for (String chatId : chatIds) {
-                log.info("callMorning chat id: " + chatId);
-                message.setChatId(chatId);
-                bot.sendMessage(message);
-            }
-            RossetiStorage.remove(formattedDate);
-        } else {
-            log.info("Morning reminder - resultOutageDto is null");
-        }
-        log.info("Morning reminder schedule finished");
-    }
-
-    @Scheduled(cron = "${scheduling.cron.evening-koltushi}", zone = "Europe/Moscow")
+    @Scheduled(cron = "${scheduling.cron.update-koltushi}", zone = "Europe/Moscow")
     public void callEveningKoltushi() {
-        log.info("Koltushi evening schedule started");
-
+        log.info("Koltushi updating schedule started");
         List<KoltushiOutageResponseDto> outages = koltushiService.request();
 
         koltushiStorage.clear();
@@ -115,6 +57,7 @@ public class ScheduledTasks {
         List<KoltushiOutageResponseDto> tomorrowOutages = outages.stream()
                 .filter(dto -> tomorrow.equals(dto.getDate()))
                 .toList();
+
         if (!tomorrowOutages.isEmpty()) {
             log.info("Outage tomorrow found, sending Telegram warning");
             SendMessage message = processor.makeKoltushiWarningMessage(tomorrowOutages);
@@ -123,6 +66,7 @@ public class ScheduledTasks {
                 bot.sendMessage(message);
             }
         }
+
         log.info("Koltushi evening schedule finished");
     }
 }
--- src/main/java/org/monkey_business/utility_supervisor/telegram/BotMessageProcessor.java
+++ src/main/java/org/monkey_business/utility_supervisor/telegram/BotMessageProcessor.java
@@ -1,15 +1,7 @@
 package org.monkey_business.utility_supervisor.telegram;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpStatus;
 import org.monkey_business.utility_supervisor.dto.KoltushiOutageResponseDto;
-import org.monkey_business.utility_supervisor.dto.RossetiResultOutageDto;
-import org.monkey_business.utility_supervisor.dto.RossetiOutageResponseDto;
-import org.monkey_business.utility_supervisor.enums.UserState;
-import org.monkey_business.utility_supervisor.config.RossetiConfig;
-import org.monkey_business.utility_supervisor.request.RossetiRequest;
-import org.monkey_business.utility_supervisor.service.RossetiService;
-import org.monkey_business.utility_supervisor.service.StateMachineService;
 import org.monkey_business.utility_supervisor.storage.KoltushiStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,7 +10,6 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,57 +18,27 @@ import java.util.stream.Collectors;
 @Component
 @Slf4j
 public class BotMessageProcessor {
-    private final RossetiService rossetiService;
-    private final StateMachineService stateMachineService;
-    private final RossetiConfig rossetiConfig;
+
     private final KoltushiStorage koltushiStorage;
-    private final String district;
-    private final String defaultSettlement;
 
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     @Autowired
-    public BotMessageProcessor(RossetiService rossetiService, StateMachineService stateMachineService,
-                                RossetiConfig rossetiConfig, KoltushiStorage koltushiStorage) {
-        this.rossetiService = rossetiService;
-        this.stateMachineService = stateMachineService;
-        this.rossetiConfig = rossetiConfig;
+    public BotMessageProcessor(KoltushiStorage koltushiStorage) {
         this.koltushiStorage = koltushiStorage;
-        district = rossetiConfig.getDistrict();
-        defaultSettlement = rossetiConfig.getStreet();
     }
 
     public SendMessage processMessage(Update update) {
         log.info("process message");
-        SendMessage message;
         String chatId = String.valueOf(update.getMessage().getChatId());
-        String text = update.getMessage().getText();
-        String currentState = stateMachineService.getCurrentState(chatId);
-        if (currentState != null && currentState.equals(UserState.AWAITING_ADDRESS.toString())) {
-            message = makeMessageBySettlement(chatId, text);
-            message = addWelcomeKeyboard(message);
-            stateMachineService.resetState(chatId);
-        } else {
-            stateMachineService.resetState(chatId);
-            message = makeWelcomeMessage(String.valueOf(chatId));
-        }
-        return message;
+        return makeWelcomeMessage(chatId);
     }
 
     public SendMessage processCallback(Update update) {
         log.info("process callback");
         String data = update.getCallbackQuery().getData();
         Long chatId = update.getCallbackQuery().getMessage().getChatId();
-        if ("set_location".equals(data)) {
-            SendMessage message = new SendMessage();
-            message.setChatId(chatId);
-            message.setText("Введите название населенного пункта");
-            stateMachineService.setUserState(chatId.toString(), UserState.AWAITING_ADDRESS.toString());
-            return message;
-        } else if ("next_date".equals(data)) {
-            SendMessage message = makeMessageBySettlement(chatId.toString(), defaultSettlement, 1);
-            return addWelcomeKeyboard(message);
-        } else if ("koltushi_outages".equals(data)) {
+        if ("koltushi_outages".equals(data)) {
             return makeKoltushiOutagesMessage(chatId.toString());
         }
         return makeWelcomeMessage(chatId.toString());
@@ -92,83 +53,16 @@ public class BotMessageProcessor {
     }
 
     public SendMessage addWelcomeKeyboard(SendMessage message) {
-        log.info("add welcome keyboard");
         InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> keyboardRows = new ArrayList<>();
-        List<InlineKeyboardButton> row1 = new ArrayList<>();
-        List<InlineKeyboardButton> row2 = new ArrayList<>();
-        List<InlineKeyboardButton> row3 = new ArrayList<>();
-        row1.add(InlineKeyboardButton.builder().text("Узнать дату ближайшего отключения").callbackData("next_date").build());
-        row2.add(InlineKeyboardButton.builder().text("Поиск по Всеволожскому району").callbackData("set_location").build());
-        row3.add(InlineKeyboardButton.builder().text("Отключения ТП-5189/ТП-7530 (3 дня)").callbackData("koltushi_outages").build());
-        keyboardRows.add(row1);
-        keyboardRows.add(row2);
-        keyboardRows.add(row3);
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        row.add(InlineKeyboardButton.builder()
+                .text("Отключения ТП-5189/ТП-7530 (3 дня)")
+                .callbackData("koltushi_outages")
+                .build());
+        keyboardRows.add(row);
         keyboardMarkup.setKeyboard(keyboardRows);
         message.setReplyMarkup(keyboardMarkup);
-        return message;
-    }
-
-    public SendMessage makeMessageBySettlement(String chatId, String settlement) {
-        log.info("make message by settlement");
-        LocalDate now = LocalDate.now();
-        LocalDate plusMonth = now.plusMonths(1);
-        RossetiRequest request = new RossetiRequest(district, now, plusMonth, settlement);
-        RossetiResultOutageDto<RossetiOutageResponseDto> rossetiResultOutageDto = rossetiService.find(request);
-        SendMessage message = processMessageText(rossetiResultOutageDto, settlement);
-        message.setChatId(chatId);
-        return message;
-    }
-
-    public SendMessage makeMessageBySettlement(String chatId, String settlement, int limit) {
-        log.info("make message by settlement with limit");
-        LocalDate now = LocalDate.now();
-        LocalDate plusMonth = now.plusMonths(1);
-        RossetiRequest request = new RossetiRequest(district, now, plusMonth, settlement);
-        RossetiResultOutageDto<RossetiOutageResponseDto> rossetiResultOutageDto = rossetiService.find(request, limit);
-        SendMessage message = processMessageText(rossetiResultOutageDto, settlement);
-        message.setChatId(chatId);
-        message.enableHtml(true);
-        return message;
-    }
-
-    private SendMessage processMessageText(RossetiResultOutageDto<RossetiOutageResponseDto> rossetiResultOutageDto, String settlement) {
-        log.info("process message text");
-        SendMessage message = new SendMessage();
-        if (rossetiResultOutageDto.getStatusCode() != HttpStatus.SC_OK) {
-            message.setText("Сервис временно недоступен, попробуйте позже");
-        } else if (rossetiResultOutageDto.getData().size() > 0) {
-            List<String> rossetiEvents = rossetiResultOutageDto.getData().stream().map(m -> m.getDistrict() + ", "
-                    + m.getAddress() + ". Плановое отключение электричества " + m.getStartDate() + " с "
-                    + m.getStartTime() + " по " + m.getEndDate() + " " + m.getEndTime()
-                    + "\n\n").collect(Collectors.toList());
-            message.setText(String.join("", rossetiEvents));
-        } else {
-            message.setText("Данные о плановых отключениях для " + settlement + " не найдены");
-        }
-        message.enableHtml(true);
-        return message;
-    }
-
-    public SendMessage makeMessageForTomorrow(RossetiResultOutageDto<RossetiOutageResponseDto> rossetiResultOutageDto) {
-        log.info("make message for tomorrow");
-        SendMessage message = new SendMessage();
-        String startTime = rossetiResultOutageDto.getData().get(0).getStartTime();
-        String endTime = rossetiResultOutageDto.getData().get(0).getEndTime();
-        String startDate = rossetiResultOutageDto.getData().get(0).getStartDate();
-        message.setText("На завтра '" + startDate + "' в д. Разметелево запланировано отключение электричества с " +
-                startTime + " до " + endTime);
-        return message;
-    }
-
-    public SendMessage makeMessageForToday(RossetiResultOutageDto<RossetiOutageResponseDto> rossetiResultOutageDto) {
-        log.info("make message for today");
-        SendMessage message = new SendMessage();
-        String startTime = rossetiResultOutageDto.getData().get(0).getStartTime();
-        String endTime = rossetiResultOutageDto.getData().get(0).getEndTime();
-        String startDate = rossetiResultOutageDto.getData().get(0).getStartDate();
-        message.setText("Внимание! Сегодня '" + startDate + "' в д. Разметелево запланировано отключение электричества с " +
-                startTime + " до " + endTime);
         return message;
     }
 
